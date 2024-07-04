d0aa8d2452034a3b43b40a7591c87bd042d0e4ad
Mizrain de Sousa <mizrains07@gmail.com>
2024-07-04 00:15:44 -0300

    hospedagem

--- umaCausa/settings.py
+++ umaCausa/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'usuarios',
     'doacoes',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'umaCausa.urls'
